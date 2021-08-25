cb536228b9c04ede8f2a0a8d8c98c7002610aba7
DESKTOP-43QRLRL\long <long0426@gmail.com>
2021-08-26 01:56:36 +0800

    init

---
+++ src/main/java/com/cathaybank/test/CathayBankTestApplication.java
@@ -0,0 +1,13 @@
+package com.cathaybank.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CathayBankTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CathayBankTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cathaybank/test/CathayBankTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cathaybank.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CathayBankTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
