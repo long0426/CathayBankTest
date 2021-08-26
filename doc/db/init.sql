6145b7a3a9505b498b141ba0e7c2db27d16760f5
DESKTOP-43QRLRL\long <long0426@gmail.com>
2021-08-26 11:11:45 +0800

    modify column name

--- doc/db/init.sql
+++ doc/db/init.sql
@@ -5,9 +5,9 @@ CREATE TABLE demo.CurrencyInfo (
 	code varchar(10) NOT NULL COMMENT '幣別代碼',
 	name varchar(100) NOT NULL COMMENT '中文名稱',
 	creator varchar(100) NOT NULL COMMENT '創建人',
-	createTime DATETIME NOT NULL COMMENT '創建時間',
+	create_time DATETIME NOT NULL COMMENT '創建時間',
 	updator varchar(100) NULL COMMENT ' 修改者',
-	updateTime DATETIME NULL COMMENT '修改時間',
+	update_time DATETIME NULL COMMENT '修改時間',
 	enable BOOL NOT NULL COMMENT '是否啟用',
 	CONSTRAINT CurrencyInfo_pk PRIMARY KEY (code)
 )
