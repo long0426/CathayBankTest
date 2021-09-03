f7400c86738cd2fc6fc2586d468e8c5a67d99561
DESKTOP-43QRLRL\long <long0426@gmail.com>
2021-09-03 16:05:45 +0800

    fix chinese garbled

--- src/test/java/com/cathaybank/test/controller/CoinDeskControllerTest.java
+++ src/test/java/com/cathaybank/test/controller/CoinDeskControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +22,7 @@ class CoinDeskControllerTest {
 	@Test
 	void testGetCurrentPrice() throws Exception {
 		mockMvc.perform(get("/coindesk/show")
+				.accept(MediaType.APPLICATION_JSON_UTF8)
 				)
         .andExpect(status().isOk())
         .andDo(print());
